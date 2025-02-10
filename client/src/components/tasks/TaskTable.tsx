30c45e5a0a733e6b714d342eccb2349701c92c3d
EvanCNavarro <3213772-EvanCNavarro@users.noreply.replit.com>
2025-02-10 19:50:10 +0000

    Agent query: Can you see the ellipsis menu button in the last column of each task row, and does clicking it show a 'View Details' option?
    Add ellipses menu to Task Center table
    
    Screenshot: https://storage.googleapis.com/screenshot-production-us-central1/9606074c-a9ad-4fe1-8fe5-3d9c3eed0606/65c5d777-c165-462f-9834-fe411ae9f466.jpg

--- client/src/components/tasks/TaskTable.tsx
+++ client/src/components/tasks/TaskTable.tsx
@@ -21,49 +21,59 @@ export function TaskTable({ tasks }: { tasks: Task[] }) {
 
   return (
     <>
-      <Table>
-        <TableHeader>
-          <TableRow>
-            <TableHead>Task</TableHead>
-            <TableHead>Status</TableHead>
-            <TableHead>Progress</TableHead>
-            <TableHead>Due Date</TableHead>
-            <TableHead className="w-[50px]"></TableHead>
-          </TableRow>
-        </TableHeader>
-        <TableBody>
-          {tasks.map((task) => (
-            <TableRow key={task.id}>
-              <TableCell>{task.title}</TableCell>
-              <TableCell>{task.status}</TableCell>
-              <TableCell>{task.progress}%</TableCell>
-              <TableCell>
-                {task.dueDate ? format(new Date(task.dueDate), 'MMM d, yyyy') : '-'}
-              </TableCell>
-              <TableCell>
-                <DropdownMenu>
-                  <DropdownMenuTrigger asChild>
-                    <Button variant="ghost" className="h-8 w-8 p-0">
-                      <MoreHorizontal className="h-4 w-4" />
-                    </Button>
-                  </DropdownMenuTrigger>
-                  <DropdownMenuContent align="end">
-                    <DropdownMenuItem 
-                      onClick={() => {
-                        setSelectedTask(task);
-                        setDetailsModalOpen(true);
-                      }}
-                    >
-                      View Details
-                    </DropdownMenuItem>
-                  </DropdownMenuContent>
-                </DropdownMenu>
-              </TableCell>
+      <div className="rounded-md border">
+        <Table>
+          <TableHeader>
+            <TableRow>
+              <TableHead>Task</TableHead>
+              <TableHead>Status</TableHead>
+              <TableHead>Progress</TableHead>
+              <TableHead>Due Date</TableHead>
+              <TableHead className="w-[70px]"></TableHead>
             </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-      
+          </TableHeader>
+          <TableBody>
+            {tasks.map((task) => (
+              <TableRow key={task.id}>
+                <TableCell className="font-medium">{task.title}</TableCell>
+                <TableCell>
+                  <div className="capitalize">{task.status}</div>
+                </TableCell>
+                <TableCell>{task.progress}%</TableCell>
+                <TableCell>
+                  {task.dueDate ? format(new Date(task.dueDate), 'MMM d, yyyy') : '-'}
+                </TableCell>
+                <TableCell className="text-right pr-4">
+                  <DropdownMenu>
+                    <DropdownMenuTrigger asChild>
+                      <Button 
+                        variant="ghost" 
+                        size="icon"
+                        className="h-8 w-8 p-0 hover:bg-accent"
+                      >
+                        <span className="sr-only">Open menu</span>
+                        <MoreHorizontal className="h-4 w-4" />
+                      </Button>
+                    </DropdownMenuTrigger>
+                    <DropdownMenuContent align="end" className="w-[160px]">
+                      <DropdownMenuItem 
+                        onClick={() => {
+                          setSelectedTask(task);
+                          setDetailsModalOpen(true);
+                        }}
+                        className="cursor-pointer"
+                      >
+                        View Details
+                      </DropdownMenuItem>
+                    </DropdownMenuContent>
+                  </DropdownMenu>
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </div>
+
       <TaskDetailsModal
         task={selectedTask}
         open={detailsModalOpen}
